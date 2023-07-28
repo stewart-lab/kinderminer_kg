afe76113d1b4c6e6ba1852b78299d0070c9009f9
ROBERT MILLIKIN <rmillikin@wisc.edu>
2023-07-28 10:59:32 -0500

    specific spacy-transformers version

--- 2_train/rel/train_model.sh
+++ 2_train/rel/train_model.sh
@@ -1,11 +1,11 @@
 # create venv
-python3 -m venv ./spans-venv
-python -m venv ./spans-venv
-pip install --upgrade pip
+#python3 -m venv ./spans-venv
+#python -m venv ./spans-venv
+#pip install --upgrade pip
 
 # install spacy
 pip install spacy==3.3.1
-pip install spacy-transformers
+pip install spacy-transformers==1.1.7
 
 # train the model
 spacy project run train_gpu
